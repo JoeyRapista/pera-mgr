06a8d3d44f2077c64b14838f8c1c8399ba37214e
Joey <yeojrapista@gmail.com>
2022-04-10 20:31:20 +0800

    tweaked for heroku

--- expense_tracker/__init__.py
+++ expense_tracker/__init__.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://xuizmjghhrjill:51769414f14626d2e5582b9cfeadbbc395eeebc52cdc216b9d3f68fe23950081@ec2-52-73-155-171.compute-1.amazonaws.com:5432/d2d7k8ttson55g'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///joey.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ouvszwdwtrpvio:7ca1c2e84226603f7ddcbda2c41caa4f1fc61a1535864ed7c11b23bc3db55c64@ec2-52-203-118-49.compute-1.amazonaws.com:5432/ddhul1vt45jme2'
 app.config['SECRET_KEY'] = "as#asdASASF#ASFLKsndvssdf2p2990$"
 
 db = SQLAlchemy(app)
